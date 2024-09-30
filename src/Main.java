5a870b29a9bb787c6d82be6ba8341c2e14e5c97a
inix84 <inix84@mail.ru>
2024-09-30 22:45:38 +0500

    доработка задач 2 и 3, про операционные системы и високосный год.

--- src/Main.java
+++ src/Main.java
@@ -18,8 +18,7 @@ public class Main {
             } else {
                 System.out.println("Установите версию приложения для iOS по ссылке --->");
             }
-        }
-        else {
+        } else {
             if (clientDeviceYear < 2015) {
                 System.out.println("Установите облегченную версию приложения для Android по ссылке--->");
             } else {
