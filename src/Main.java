51b5f4598a826044a2b3dced776dd57ace02582f
inix84 <inix84@mail.ru>
2024-09-30 18:20:40 +0500

    доработка задач 2 и 3, про операционные системы и високосный год.

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,41 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Задача 1. Про операционные системы:");
-        int clientOS = 1; //0 — iOS, 1 — Android
+        int clientOS = 0; //0 — iOS, 1 — Android
         if (clientOS == 0) {
             System.out.println("Установите версию приложения для iOS по ссылке --->");
         } else if (clientOS == 1) {
             System.out.println("Установите версию приложения для Android по ссылке --->");
         }
         System.out.println();
+
+
         System.out.println("Задача 2. Про операционные системы (хард):");
         int clientDeviceYear = 2014; // год выпуска
-        if (clientOS == 0 && clientDeviceYear >= 2015) {
-            System.out.println("Установите версию приложения для iOS по ссылке--->");
-        } else if (clientOS == 0 && clientDeviceYear < 2015) {
-            System.out.println("Установите облегченную версию приложения для iOS по ссылке--->");
+        if (clientOS == 0) {
+            if (clientDeviceYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке--->");
+            } else {
+                System.out.println("Установите версию приложения для iOS по ссылке --->");
+            }
+        }
+        else {
+            if (clientDeviceYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке--->");
+            } else {
+                System.out.println("Установите версию приложения для Android по ссылке --->");
+            }
         }
 
-        if (clientOS == 1 && clientDeviceYear >= 2015) {
-            System.out.println("Установите версию приложения для Android  по ссылке--->");
-        } else if (clientOS == 1 && clientDeviceYear < 2015) {
-            System.out.println("Установите облегченную версию приложения для Android по ссылке--->");
-        }
         System.out.println();
         System.out.println("Задача 3. Про високосный год:");
-        int year = 2021; // проверяемый год
-        if (year % 4 == 0 && year % 100 != 0) {
-            System.out.println(year + " год является весьма високосным!");
-        } else {
+        int year = 2000; // проверяемый год
+        if (year % 4 != 0) {
+            System.out.println(year + " год не является високосным!");
+        } else if (year % 100 == 0 && year % 400 != 0) {
             System.out.println(year + " год не является високосным, как на него не посмотри.");
+        } else {
+            System.out.println(year + " год на зло всем бедам является високосным.");
         }
         System.out.println();
         System.out.println("Задача 4. Про доставку банковских карт:");
@@ -72,7 +80,5 @@ public class Main {
             default:
                 System.out.println("Неверно введен номер месяца.");
         }
-
-
     }
 }
